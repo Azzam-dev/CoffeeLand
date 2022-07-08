b73c69564fb41962e3e75da61453b051f9e7e7c9
CAOCAP <ficruty@gmail.com>
2022-07-08 13:17:58 +0300

    worked on saving the orders in localStorage

--- src/pages/NewOrder.jsx
+++ src/pages/NewOrder.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import "./NewOrder.css";
 
 const NewOrder = () => {
   const indexNames = ["First", "Second", "Third", "Fourth", "Fifth"];
+  const [tableNumber, setTableNumber] = useState("");
   const [orderItemsList, setOrderItemsList] = useState([""]);
+
+  const ordersList = (() => {
+    const list = localStorage.getItem("orders");
+    return list === null ? {} : JSON.parse(list);
+  })();
 
   const handleChangeItem = (item, index) => {
     const list = [...orderItemsList];
@@ -23,6 +29,15 @@ const NewOrder = () => {
     setOrderItemsList([...orderItemsList, ""]);
   };
 
+  const handleSaveOrder = () => {
+    ordersList[tableNumber] = orderItemsList;
+    localStorage.setItem("orders", JSON.stringify(ordersList));
+  };
+
+  useEffect(() => {
+    console.log(JSON.parse(localStorage.getItem("orders")));
+  }, []);
+
   return (
     <div className="main">
       <h1>Form</h1>
@@ -30,7 +45,11 @@ const NewOrder = () => {
         <Form className="p-2 w-100">
           <Form.Group className="mb-3 ">
             <Form.Label>Table number</Form.Label>
-            <Form.Control required />
+            <Form.Control
+              required
+              value={tableNumber}
+              onChange={(e) => setTableNumber(e.target.value)}
+            />
             <Form.Text className="text-muted">
               You can find the number on your table.
             </Form.Text>
@@ -75,7 +94,7 @@ const NewOrder = () => {
             <Form.Check type="checkbox" label="Check me out" />
           </Form.Group>
           <div className="d-grid">
-            <Button variant="dark" type="submit">
+            <Button variant="dark" type="submit" onClick={handleSaveOrder}>
               Save Order
             </Button>
           </div>
